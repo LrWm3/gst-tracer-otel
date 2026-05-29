9239fb844f0ce53b7bc1448878cc7e680671f54b
LrWm3 <William.Marsman@gmail.com>
2026-05-29 11:30:30 -0300

    chore: add a few more intervals for the histogram

--- tracer/prometheus/src/promlatencyimp.rs
+++ tracer/prometheus/src/promlatencyimp.rs
@@ -50,8 +50,8 @@ static LATENCY_HISTOGRAM: LazyLock<HistogramVec> = LazyLock::new(|| {
         "Latency histogram in seconds per element",
         &["element", "src_pad", "sink_pad", "path"],
         vec![
-            0.001, 0.0025, 0.005, 0.01, 0.025, 0.05, 0.075, 0.1, 0.25, 0.5, 0.75, 1.0, 2.5, 5.0,
-            10.0, 15.0, 30.0, 60.0,
+            0.00001, 0.0001, 0.001, 0.0025, 0.005, 0.01, 0.025, 0.05, 0.075, 0.1, 0.25, 0.5, 0.75,
+            1.0, 2.5, 5.0, 10.0, 20.0, 60.0, 240.0,
         ]
     )
     .unwrap()
